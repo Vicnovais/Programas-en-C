4abc5432227247f7290f852b0b4df70b66fa715a
Vicnovais <vicnovais@gmail.com>
2015-04-22 20:44:46 -0300

    Update struct.c
    Added a printf to show a example of struct elements sum.

--- structs.c
+++ structs.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Create struct
 struct point {
 	int x;
 	int y;
@@ -10,8 +11,9 @@ void main(){
 	struct point p2 = {7,8};
 	p1.x = 3;
 	p2.y = 666;
-	printf("las coordenadas son p1.x=%d y p1.y=%d\n", p1.x, p1.y);
-	printf("las coordenadas son p2.x=%d y p2.y=%d\n", p2.x, p2.y);
+	printf("The coordinates are p1.x=%d y p1.y=%d\n", p1.x, p1.y);
+	printf("The coordinates are p2.x=%d y p2.y=%d\n", p2.x, p2.y);
+	printf("The sum of x coordinates are: %d", (p1.x+p2.x));
 
 	printf("dir x=%d dir y=%d",&p1.x,&p1.y);
 }
